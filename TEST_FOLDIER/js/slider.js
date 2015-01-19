41c4d272e64733ad6e370682b99b4929e4afce84
CharlyCB <carlos.coloborde@gmail.com>
2015-01-19 13:05:11 +0100

    Slider 001 con imagenes OP

--- TEST_FOLDIER/js/slider.js
+++ TEST_FOLDIER/js/slider.js
@@ -1,21 +1,28 @@
-function bigslide(){
+
+
+
+	function bigslide(){
 		document.getElementById("slider").className = "slide_max";
 		document.getElementById('close').className = "close";
 	}
+	
 	function bigimg(target){
-		for(var a = 0; a < 10; a++){
-			if( a == target){
+		var id = target.replace('spid','');
+		for(var a = 1; a < 11; a++){
+			if( 'spid'+a == target){
 				document.getElementById(target).className = "minislide target";
 			}
 			else{
-				document.getElementById(a).className = "minislide notarget";
+				document.getElementById('spid'+a).className = "minislide notarget";
 			}
 		}
 	}
+	
+	
 	function closeslider(){
 		document.getElementById("slider").className = "slide_min";
 		document.getElementById('close').className = "";
-		for(var a = 0; a < 10; a++){
-				document.getElementById(a).className = "minislide";
+		for(var a = 1; a < 11; a++){
+				document.getElementById('spid'+a).className = "minislide";
 		}
 	}
