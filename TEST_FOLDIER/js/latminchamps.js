bffba646386e09509de901decd0f6e9cbbfa8142
gabrimagic <gabrielovo1500@hotmial.com>
2015-01-20 02:57:33 +0000

    Index, cosas nuevas

--- TEST_FOLDIER/js/latminchamps.js
+++ TEST_FOLDIER/js/latminchamps.js
@@ -1,6 +1,11 @@
 
 	function latmidchamps(){
+
+
 		for (var i = 1; i <= 123; i++){
-			document.getElementById("right_bar").innerHTML += "<div id=\""+i+"\"class=\"div_mini\"><img src=\"images/champs_mini/mid_"+i+".png\" width=\"70\" height=\"70\" /></div>";
+
+		document.getElementById("right_bar").innerHTML += 
+		"<div id=\""+i+"\"class=\"div_mini\"><img src=\"images/champs_mini/mid_"+i+".png\" width=\"70\" height=\"70\" /></div>";
+
 		}
 }
