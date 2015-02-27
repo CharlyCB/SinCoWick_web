fa04786523c8a1c7fcdf80724cae7a4c4893537e
CharlyCB <carlos.coloborde@gmail.com>
2015-02-27 21:51:42 +0100

    slider sefini

--- TEST_FOLDIER/js/slider.js
+++ TEST_FOLDIER/js/slider.js
@@ -7,9 +7,7 @@
 	}
 	
 	function bigimg(target){
-		//ADASDASDASDA
-		//Esta parte no la toques:
-		/*
+
 		$.ajax({
 			url: 'http://gehena.byethost7.com/pruebasincowick/try001.php',
 			cache: false,
@@ -22,27 +20,46 @@
 				for(var i = 0; i < 10; i++){
 					id[i] = datos.champions[i].id;
 				}
+				
+				for(var a = 0; a < 10; a++){
+					if( 'spid'+id[a] == target){
+						document.getElementById(target).className = "minislide target";
+					}
+					else{
+						document.getElementById('spid'+id[a]).className = "minislide notarget";
+					}
+				}
+				
+				
 			}
 		});		
 		
-		*/
-		//ASDASDASDASAD
-		var id = target.replace('spid','');
-		for(var a = 1; a < 500; a++){
-			if( 'spid'+a == target){
-				document.getElementById(target).className = "minislide target";
-			}
-			else{
-				document.getElementById('spid'+a).className = "minislide notarget";
-			}
-		}
 	}
 	
 	
 	function closeslider(){
 		document.getElementById("slider").className = "slide_min";
 		document.getElementById('close').className = "";
-		for(var a = 1; a < 11; a++){
-				document.getElementById('spid'+a).className = "minislide";
-		}
+		
+		$.ajax({
+			url: 'http://gehena.byethost7.com/pruebasincowick/try001.php',
+			cache: false,
+			type: "GET",
+			//LOS DATOS SON EL RESPONSE ECHO JSON
+			success: function(datos){
+				datos = JSON.parse(datos);
+				
+				var id = [];
+				for(var i = 0; i < 10; i++){
+					id[i] = datos.champions[i].id;
+				}
+				
+				for(var a = 0; a < 10; a++){
+					document.getElementById('spid'+id[a]).className = "minislide";
+				}
+				
+				
+			}
+		});		
+		
 	}
